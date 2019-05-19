482b9e7c3ac806e472e6b28683be5c7bcdd81762
hishinuma616 <dnbwoodleaves3616@gmail.com>
2019-05-20 01:00:18 +0900

    firstCommit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const url = require('url')
+const app = express();
+app.use(bodyParser.json({
+  limit: bytes.parse('50mb'),
+  extended: true
+}));
+const aws = require('aws-sdk');
+aws.config.update({
+  accessKeyId: process.env.awsAccessKeyId,
+  secretAccessKey: process.env.awsSecretAccessKey,
+  region: 'ap-northeast-1'
+});
+const s3 = new aws.S3();
+
+//クロスドメイン許可
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+async function getFileName(name) {
+  return new Promise((resolve) => {
+
+    const params2 = {
+      Bucket: 'mrble-portfolio',
+      Prefix: 'purichan/' + name + '/'
+    }
+
+    s3.listObjectsV2(params2, (err, data) => {
+      if (err) {
+        console.log("Error", err)
+        resolve('Error')
+      } else {
+        const fileList = data.Contents
+        fileList.shift()
+        resolve(fileList[Math.floor(Math.random() * fileList.length )].Key)
+      }
+    })
+  })
+}
+
+app.get('/getFileName', (req, res) => {
+  const name = req.query.name
+  let filePath
+  switch(name) {
+    case 'mirai':
+    case 'emo':
+    case 'rinka':
+      getFileName(name).then((result) => {
+        res.send(result)
+      })
+    break
+    default:
+      res.send('存在しないリクエスト')
+      break
+  }
+})
+
+module.exports = {
+  path: '/api',
+  handler: app,
+}
